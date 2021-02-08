ffa94478ee413c20c5b254d5305c9d71d571ce94
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-08 18:30:04 +0100

    add paintList function and paint movies title

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,34 @@
 const inputEl = document.querySelector(".js-input");
 const buttonEl = document.querySelector(".js-button");
 //const apiurl = 'http://api.tvmaze.com/search/shows?q='
-const ulLI = document.querySelector(".js-showList");
+const ulElement = document.querySelector(".js-showList");
 const inputValue = inputEl.value;
 
 let showsList = [];
 
-function getApiData(ev) {
-  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+function getApiData() {
+  fetch(`http://api.tvmaze.com/search/shows?q= ${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
-        showsList = data[i].show;
+        showsList.push(data[i].show);
       }
     });
+  paintList();
 }
 buttonEl.addEventListener("click", getApiData);
+
+// paint
+function paintList() {
+  let html = "";
+  for (const item of showsList) {
+    html += "<div class = 'container'>";
+    html += "<li class = 'shows'>";
+    html += `<h2 class="shows__title">${item.name}</h2>`;
+  }
+  html += "</li>";
+  html += "</div>";
+  ulElement.innerHTML = html;
+}
+
+getApiData();
