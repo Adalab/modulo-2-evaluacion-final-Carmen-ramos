105bbbd7d6b221b9039ecf7702744c051566043c
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-08 21:32:23 +0100

    paint images and create a image default

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@ const inputEl = document.querySelector(".js-input");
 const buttonEl = document.querySelector(".js-button");
 //const apiurl = 'http://api.tvmaze.com/search/shows?q='
 const ulElement = document.querySelector(".js-showList");
-const inputValue = inputEl.value;
 
 let showsList = [];
 
 function getApiData() {
-  fetch(`http://api.tvmaze.com/search/shows?q= ${inputValue}`)
+  const inputValue = inputEl.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
@@ -19,17 +19,33 @@ function getApiData() {
 }
 buttonEl.addEventListener("click", getApiData);
 
+//preguntar form
+/*const formElement = document.querySelector(".js-form");
+function handleForm(ev) {
+  ev.preventDefault();
+}
+formElement.addEventListener("submit", handleForm);*/
+
+// Image default for shows without image.
+let imgDefault = "././assets/images/tvSeries.jpg";
+
 // paint
 function paintList() {
   let html = "";
   for (const item of showsList) {
     html += "<div class = 'container'>";
-    html += "<li class = 'shows'>";
+    html += `<li class = "shows" id=${item.id}>`;
     html += `<h2 class="shows__title">${item.name}</h2>`;
+    if (item.image === null) {
+      html += `<img src= ${imgDefault} class= "imgDefault">`;
+    } else {
+      html += `<img src= ${item.image.medium}>`;
+    }
+    html += "</li>";
+    html += "</div>";
   }
-  html += "</li>";
-  html += "</div>";
   ulElement.innerHTML = html;
 }
 
 getApiData();
+/*handleForm();*/
