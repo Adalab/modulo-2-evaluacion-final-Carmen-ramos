3e6f23706d7f4c3571e264cf15f571725aca9082
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-08 17:00:10 +0100

    first steps - call api, take data and save in let - inside function

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+const inputElement = document.querySelector(".js-input");
+const buttonElement = document.querySelector(".js-button");
+
+let shows = [];
+
+function getApiData() {
+  fetch("http://api.tvmaze.com/search/shows?q=girls")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        shows = data[i].show;
+      }
+    });
+}
